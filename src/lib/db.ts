23352a7cc9f22aad12e94c37aade2cb2a18befa9
josehenriquedasilva <josehenrique60t@gmail.com>
2026-03-16 15:31:07 -0300

    Ajuste prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,11 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
-};
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient(); // No P7, ele busca a config do prisma.config.ts automaticamente
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
