8664204a44fb71993588b5cf66e7e8c473e4744b
josehenriquedasilva <josehenrique60t@gmail.com>
2026-03-16 16:38:17 -0300

    Ajuste prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient(); 
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
