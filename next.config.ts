c858d7bc701127210dc3c903ac3d0c5f5a25a72c
josehenriquedasilva <josehenrique60t@gmail.com>
2026-03-16 16:57:41 -0300

    Ajuste prisma

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: "standalone",
 };
 
 export default nextConfig;
