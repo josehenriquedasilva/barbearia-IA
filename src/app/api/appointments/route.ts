883e87cfd105c2cd7ca131d045a411d56476d231
josehenriquedasilva <josehenrique60t@gmail.com>
2026-03-17 10:05:46 -0300

    Ajuste prisma

--- src/app/api/appointments/route.ts
+++ src/app/api/appointments/route.ts
@@ -2,6 +2,8 @@ import prisma from "@/lib/db";
 import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
